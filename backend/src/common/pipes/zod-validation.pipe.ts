27a54f5f2bc3f7b101386e5efdb7aadd780e16a1
Nocturnale <arnaud.douillard@gmail.com>
2026-09-24 11:26:00 +0200

    feat: create/update/get platform complete

---
+++ backend/src/common/pipes/zod-validation.pipe.ts
@@ -0,0 +1,21 @@
+import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
+import { ZodError, ZodType } from 'zod';
+
+@Injectable()
+export class ZodValidationPipe implements PipeTransform {
+    //difference zodType et zod Schema
+  constructor(private readonly schema: ZodType) {}
+
+  transform(value: unknown) {
+    const result = this.schema.safeParse(value);
+
+    if (!result.success) {
+      throw new BadRequestException({
+        message: 'Validation failed',
+        errors: result.error.issues,
+      });
+    }
+
+    return result.data;
+  }
+}
